5778761c64afa992755a24f0f47ffd74b7e2496e
Billy <1403809057@qq.com>
2019-01-09 17:39:04 +0800

    coding: methods

--- src/utils/mixin.js
+++ src/utils/mixin.js
@@ -1,7 +1,16 @@
 export default {
     data(){
         return {
-            loading: false,
+            loading: false,//默认为true
+            visibleSearch: false,
+            currentPage: 1,//当前页数
+            pageSize: 10,//页数大小
+            total: 0,//数据总数
+        }
+    },
+    methods:{
+        showSearch(){
+            this.visibleSearch = !this.visibleSearch;
         }
     }
 }
--- src/views/goods/list.vue
+++ src/views/goods/list.vue
@@ -11,21 +11,21 @@
                         <i class="el-icon-search">筛选查询</i>
                     </div>
                     <div>
-                        <i class="el-icon-arrow-down">打开筛选</i>
-                        <i class="el-icon-arrow-up">收起筛选</i>
+                        <i class="el-icon-arrow-down" @click="showSearch" v-show="!visibleSearch">打开筛选</i>
+                        <i class="el-icon-arrow-up" @click="showSearch" v-show="visibleSearch">收起筛选</i>
                         <el-button>查询结果</el-button>
                     </div>
                 </div>
-                <div class="filter-down">
+                <div class="filter-down" v-show="visibleSearch">
                     <span>输入搜索:</span>
-                    <el-input v-model="goodsName"></el-input>
+                    <el-input v-model="goodsName" placeholder="商品名称/商品货号"></el-input>
                     <span>选择分类:</span>
-                    <el-select>
-                        <el-option>1</el-option>
+                    <el-select v-model="typeId" placeholder="请选择商品分类">
+                        <el-option v-for="item in categoryList" :label="item.typeName" :value="item.id"></el-option>
                     </el-select>
                     <span>选择品牌:</span>
-                    <el-select>
-                        <el-option>2</el-option>
+                    <el-select v-model="brandId" placeholder="请选择商品品牌">
+                        <el-option v-for="item in brandList" :label="item.name" :value="item.id"></el-option>
                     </el-select>
                 </div>
             </div>
@@ -37,7 +37,7 @@
                 </div>
             </div>
             <!--数据区域-->
-            <el-table class="mall-table" :data="goodsList" v-loading="loading">
+            <el-table class="mall-table" :data="tableData" v-loading="loading">
                 <el-table-column type="selection" prop="id"></el-table-column>
                 <el-table-column label="编号" prop="id"></el-table-column>
                 <el-table-column label="商品图片">
@@ -82,9 +82,9 @@
             <!--库存编辑弹窗-->
             <el-dialog title="编辑货品信息" :visible.sync="dialogVisible" :append-to-body="true">
                 <div class="dialog-header" v-loading="">
-                   <div>
-                       <p>商品货号:{{goodsNo}}</p>
-                   </div>
+                    <div>
+                        <p>商品货号:{{goodsNo}}</p>
+                    </div>
                     <table>
                         <thead>
                         <td>item</td>
@@ -95,9 +95,15 @@
                         <tbody>
                         <tr>
                             <td>大衣:女</td>
-                            <td><el-input type="number"></el-input></td>
-                            <td><el-input type="number"></el-input></td>
-                            <td><el-input type="number"></el-input></td>
+                            <td>
+                                <el-input type="number"></el-input>
+                            </td>
+                            <td>
+                                <el-input type="number"></el-input>
+                            </td>
+                            <td>
+                                <el-input type="number"></el-input>
+                            </td>
                         </tr>
                         </tbody>
                     </table>
@@ -114,7 +120,7 @@
 <script>
     import subTitle from '../../components/subTitle'
     import pagination from '../../components/pagination'
-    import mixin from  '../../utils/mixin'
+    import mixin from '../../utils/mixin'
 
     export default {
         name: "list",
@@ -125,8 +131,7 @@
         },
         data() {
             return {
-                //测试用数据↓
-                goodsList: [
+                tableData: [
                     {
                         id: 52131,
                         goodsImg: '@/assets/image.png',
@@ -268,17 +273,68 @@
                         status: 1
                     },
                 ],
-                dialogVisible: true,
                 goodsNo: 'abcdefa95dag47gagd245d2',
-                //测试用数据↑
+
+                //搜索数据
 
                 goodsName: '',
+                typeId: '',
+                brandId: '',
+
+                //其他数据
+                dialogVisible: false,
+                categoryList: [{typeName: '大衣', id: 0}, {typeName: '长裙', id: 1}],
+                brandList: [{name: '安踏', id: 0}, {name: '贵人鸟', id: 1}],
+            }
+        },
+        mounted() {
+            //初始化筛选框中的分类列表
+            /*this.$ajax.post('merchantGoodsType/query_goods_type_tree').then((res) => {
+                this.categoryList = res;
+            });*/
+            //初始化筛选框中的品牌列表
+            /*this.$ajax.post('merchant_goods_brand/query_list').then((res) => {
+                this.brandList = res;
+            });*/
+            //初始化表格数据
+            //this.getList();
+        },
+        methods: {
+            //从服务器获取表格数据
+            /*getList(){
+                //修改mixin里的loading属性，表示正在加载
+                this.loading = true;
+                //向服务器请求数据
+                this.$ajax.post('merchantGoods/merchant_goods_list_page',{
+                    //上传的数据
+                    currentPage: this.currentPage,
+                    pageSize: this.pageSize,
+                    goodsName: this.goodsName,
+                    typeId: this.typeId,
+                    brandId: this.brandId
+                }).then((res) => {
+                    //处理服务器相应
+                    this.total = res.totalCount;
+                    this.tableData = res.list;
+                },(err) => {
+                    //请求错误的处理
+                    this.$msgErr(err.msg);
+                }).finally(() => {
+                    //结束加载状态
+                    this.loading = false;
+                })
+            },*/
+            editStock(id){
+
             }
         }
     }
 </script>
 
 <style scoped lang="scss">
-
-
+    .filter-down {
+        overflow: hidden;
+        border: 1px solid #ddd;
+        box-sizing: border-box;
+    }
 </style>
