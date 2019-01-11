6ddfa518313a879646ae83ee04bcec537c48927b
Billy <1403809057@qq.com>
2019-01-11 17:42:55 +0800

    new data

--- src/utils/mixin.js
+++ src/utils/mixin.js
@@ -6,6 +6,7 @@ export default {
             currentPage: 1,//当前页数
             pageSize: 10,//页数大小
             total: 0,//数据总数
+            isAdd: true,//是否添加(true)/修改(false)商品
         }
     },
     methods:{
