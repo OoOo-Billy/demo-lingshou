780547d08e8ebd180f8b8a46995cf0b66e971010
billy-pad <1403809057@qq.com>
2019-02-02 16:21:36 +0800

    finished template & style & base data() {}

--- src/views/order/orderDetail.vue
+++ src/views/order/orderDetail.vue
@@ -1,19 +1,287 @@
 <template>
     <div>
-        <subTitle :subTitle="'订单详情'"/>
+        <subTitle :subTitle="'订单详情'" :isBack="true"/>
+        <div class="body">
+            <!--steps-->
+            <el-steps :active="datalist.status" align-center finish-status="success">
+                <el-step title="提交订单" :description="datalist.creatTime"></el-step>
+                <el-step title="支付订单" :description="datalist.finishTime"></el-step>
+                <el-step title="平台发货" :description="datalist.sendEndTime"></el-step>
+                <el-step title="确认收货" :description="datalist.creatTime"></el-step>
+                <el-step title="完成评价" :description="datalist.finishEndTime"></el-step>
+            </el-steps>
+
+            <!---->
+            <div class="order-info clearFix">
+                <p>
+                    <i class="el-icon-warning font-15 red">当前订单状态: {{ datalist.status | getOrderStatus }}</i>
+                </p>
+                <div>
+                    <button class="mall-btn" @click="dialogVisible = true; dialogStatus = 1; title = '修改收货人信息';">
+                        修改收货人信息
+                    </button>
+                    <button class="mall-btn" v-if="datalist.status === 1" @click="close">关闭订单</button>
+                    <button class="mall-btn" @click="dialogVisible = true; dialogStatus = 2; title = '备注订单'">备注订单
+                    </button>
+                </div>
+            </div>
+
+            <!---->
+            <div class="table flex">
+                <p class="title">基本信息</p>
+                <div>
+                    <div>订单id</div>
+                    <div>{{ datalist.id }}</div>
+                </div>
+                <div>
+                    <div>订单编号</div>
+                    <div>{{ datalist.code }}</div>
+                </div>
+                <div>
+                    <div>用户账号</div>
+                    <div>{{ mobilePhone }}</div>
+                </div>
+                <div>
+                    <div>支付方式</div>
+                    <div>微信小程序支付</div>
+                </div>
+                <div>
+                    <div>订单来源</div>
+                    <div>小程序</div>
+                </div>
+                <div>
+                    <div>订单类型</div>
+                    <div>{{ datalist.type === 1 ? '普通类型' : '限时秒杀' }}</div>
+                </div>
+                <div>
+                    <div>配送方式</div>
+                    <div>{{ datalist.sendCompany }}</div>
+                </div>
+                <div>
+                    <div>物流单号</div>
+                    <div>{{ datalist.sendCode }}</div>
+                </div>
+                <div>
+                    <div>自动确认收货时间</div>
+                    <div>{{ datalist.finishEndTime }}</div>
+                </div>
+            </div>
+
+            <!---->
+            <div class="table flex">
+                <p class="title">收货人信息</p>
+                <div>
+                    <div>收货人</div>
+                    <div>{{ datalist.receiver }}</div>
+                </div>
+                <div>
+                    <div>手机号码</div>
+                    <div>{{ datalist.phone }}</div>
+                </div>
+                <div>
+                    <div>邮政编码</div>
+                    <div></div>
+                </div>
+                <div>
+                    <div>收货地址</div>
+                    <div>{{ datalist.address }}</div>
+                </div>
+            </div>
+
+            <!---->
+            <p class="box-title font-20">商品信息</p>
+            <el-table class="mall-table" :data="datalist.details" :header-cell-style="headerStyle" :cell-style="tdStyle" max-height="800">
+                <el-table-column label="商品图片">
+                    <template slot-scope="scope">
+                        <img :src="getGoodImg(scope.row.goodsPic)" alt="">
+                    </template>
+                </el-table-column>
+                <el-table-column label="商品名称" prop="goodsName"></el-table-column>
+                <el-table-column label="价格/货号">
+                    <template slot-scope="scope">
+                        {{ scope.row.goodsPrice }}/{{ scope.row.goodsNo }}
+                    </template>
+                </el-table-column>
+                <el-table-column label="属性" prop="goodsDesc"></el-table-column>
+                <el-table-column label="数量" prop="buyNum"></el-table-column>
+                <el-table-column label="品牌" prop="goodsBrand"></el-table-column>
+                <el-table-column label="小计" prop="totalMoney"></el-table-column>
+            </el-table>
+
+            <!---->
+            <div class="table flex">
+                <p class="title">费用信息</p>
+                <div>
+                    <div>商品合计</div>
+                    <div>{{ datalist.totalMoeny }}</div>
+                </div>
+                <div>
+                    <div>运费</div>
+                    <div>包邮</div>
+                </div>
+                <div>
+                    <div>订单总金额</div>
+                    <div>{{ datalist.totalMoeny }}</div>
+                </div>
+                <div>
+                    <div>应付款金额</div>
+                    <div>{{ datalist.totalMoeny }}</div>
+                </div>
+            </div>
+
+            <!---->
+            <el-dialog
+                :title="title"
+                :visible.sync="dialogVisible"
+                :append-to-body="true"
+                width="30%">
+                <el-form :model="ruleForm" :rules="rules1" ref="ruleForm" label-width="100px" class="form">
+                    <div v-if="dialogStatus === 1">
+                        <el-form-item label="收货人姓名: " prop="receiver">
+                            <el-input v-model="ruleForm.receiver"></el-input>
+                        </el-form-item>
+                        <el-form-item label="手机号码: " prop="phone">
+                            <el-input v-model="ruleForm.phone"></el-input>
+                        </el-form-item>
+                        <el-form-item label="详细地址: " prop="address">
+                            <el-input v-model="ruleForm.address"></el-input>
+                        </el-form-item>
+                    </div>
+                    <div v-if="dialogStatus === 2">
+                        <el-form-item label="操作备注: ">
+                            <el-input type="textarea" v-model="mechantRemark" :rows="5" resize="none"></el-input>
+                        </el-form-item>
+                    </div>
+                </el-form>
+                <span slot="footer" class="dialog-footer">
+                     <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="submitForm('ruleForm')">确 定</el-button>
+                </span>
+            </el-dialog>
+        </div>
     </div>
 </template>
 
 <script>
     import subTitle from '@/components/subTitle'
+    import filters from '@/utils/filters'
+    import mixin from '@/utils/mixin'
     export default {
         name: "orderDetail",
+        filters: filters,
+        mixins: [mixin],
         components: {
             subTitle,
+        },
+        data() {
+            return {
+                datalist: {
+                    id: '',
+                    code: '',
+                    type: 1,
+                    sendCompany: '',
+                    sendCode: '',
+                    receiver: '',
+                    phone: '',
+                    address: '',
+                    status: 0,
+                    creatTime: '',
+                    finishTime: '',
+                    sendEndTime: '',
+                    finishEndTime: '2018-12-31',
+                    details: [
+                        {
+                            goodsPic: '',
+                            goodsName: '1',
+                            goodsPrice: '2',
+                            goodsNo: '3',
+                            goodsDesc: '4',
+                            buyNum: '5',
+                            goodsBrand: '6',
+                            totalMoney: '7',
+                        },
+                    ],
+                    totalMoeny: '',
+                },
+                mobilePhone: '',
+
+                //修改收货人信息dialog
+                dialogVisible: false,
+                dialogStatus: 1,
+                title: '',
+                mechantRemark: '',
+
+                ruleForm: {
+                    receiver: '',
+                    phone: '',
+                    address: '',
+                },
+                rules1: {
+                    receiver: [],
+                    phone: [],
+                    address: [],
+                }
+            }
+        },
+        methods: {
+            getList() {
+            },
+            close() {
+            },
+            submitForm() {
+            },
         }
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    @import "~@/assets/css/common";
+    .el-steps {
+        margin-bottom: 30px;
+    }
+    .order-info{
+        background-color: #efefef;
+        padding: 20px;
+        border: $border;
+        p{
+            line-height: 38px;
+        }
+    }
+    .table{
+        margin: 30px 0;
+        border: $border;
+        .title{
+            width: 100%;
+            border-bottom: $border;
+            line-height: 40px;
+            font-weight: bold;
+            padding-left: 20px;
+            box-sizing: border-box;
+            background-color: #EFEFEF;
+        }
+        >div{
+            flex: 1;
+            border-right: $border;
+            &:last-child{
+                border-right: none;
+            }
+            >div{
+                line-height: 49px;
+                font-size: 15px;
+                text-align: center;
+                &:first-child{
+                    color: $gray;
+                    font-weight: 700;
+                    background-color: #F6F6F6;
+                    border-bottom: $border;
+                }
+            }
+        }
+    }
 
+    .dialog-footer {
+        button:last-child {
+            margin-right: 0;
+        }
+    }
 </style>
