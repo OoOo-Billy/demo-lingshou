4f1ec6f83621831086c5d508e664b81bc7429895
billy-pad <1403809057@qq.com>
2019-02-02 16:20:16 +0800

    export new filter

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -4,10 +4,24 @@ const propSelectList = {
     '2': '复选属性',
 };
 
+const orderStatus = {
+    '0': '已关闭',
+    '1': '待付款',
+    '2': '待发货',
+    '3': '待收货',
+    '4': '待评价',
+    '5': '已完成',
+};
+
 function getPropSelect(param) {
     return propSelectList[param]
 }
 
+function getOrderStatus(param) {
+    return orderStatus[param]
+}
+
 export default {
     getPropSelect: getPropSelect,
+    getOrderStatus: getOrderStatus,
 }
