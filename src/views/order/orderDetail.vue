95bbbf32fcf1ebeb04c51d18f4f0ac5705bdb118
billy-pad <1403809057@qq.com>
2019-02-05 15:23:38 +0800

    modify template tags

--- src/views/order/orderDetail.vue
+++ src/views/order/orderDetail.vue
@@ -155,8 +155,8 @@
                     </div>
                 </el-form>
                 <span slot="footer" class="dialog-footer">
-                     <el-button @click="dialogVisible = false">取 消</el-button>
-          <el-button type="primary" @click="submitForm('ruleForm')">确 定</el-button>
+                    <el-button @click="dialogVisible = false">取 消</el-button>
+                    <el-button type="primary" @click="submitForm('ruleForm')">确 定</el-button>
                 </span>
             </el-dialog>
         </div>
