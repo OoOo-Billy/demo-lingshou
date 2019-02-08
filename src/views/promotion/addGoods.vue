19426150caefd7ffe198a6fa6d5f7eff36b83383
billy-pad <1403809057@qq.com>
2019-02-08 13:31:01 +0800

    init

---
+++ src/views/promotion/addGoods.vue
@@ -0,0 +1,14 @@
+<template>
+    
+</template>
+
+<script>
+    import subTitle from "@/components/subTitle"
+    export default {
+        name: "addGoods"
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
