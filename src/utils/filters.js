8f21e3d60a7b21e483adae9e52e97ea3577dd856
billy-pad <1403809057@qq.com>
2019-02-10 14:02:40 +0800

    writed new filters:getPromotionStatus & getPromotionOrderStatus

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -13,6 +13,28 @@ const orderStatus = {
     '5': '已完成',
 };
 
+const promotionStatus = {
+    '1': '拼团成功',
+    '2': '拼团失败',
+    '3': '后台取消',
+    '4': '拼团中',
+};
+
+const promotionOrderStatus = {
+    '0': '已关闭',
+    '1': '待付款',
+    '2': '待发货',
+    '3': '待收货',
+    '4': '待评价',
+    '5': '已完成',
+    '6': '拼团中',
+    '7': '拼团成功',
+    '8': '拼团取消',
+    '9': '拼团失败',
+    '10': '后台取消',
+    '20': '已删除',
+};
+
 function getPropSelect(param) {
     return propSelectList[param]
 }
@@ -21,7 +43,18 @@ function getOrderStatus(param) {
     return orderStatus[param]
 }
 
+function getPromotionStatus(param) {
+    return promotionStatus[param]
+}
+
+function getPromotionOrderStatus(param) {
+    return promotionOrderStatus[param]
+}
+
 export default {
     getPropSelect: getPropSelect,
     getOrderStatus: getOrderStatus,
+    getPromotionStatus: getPromotionStatus,
+    promotionStatus: promotionStatus,
+    getPromotionOrderStatus: getPromotionOrderStatus
 }
