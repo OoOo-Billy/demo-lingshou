701f26264e7405198f90d4737605b86d15f9bc7c
billy-pad <1403809057@qq.com>
2019-02-12 15:10:36 +0800

    add new component(unused)

---
+++ src/views/statistics/deal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <sub-title :subTitle="'交易统计'"></sub-title>
+    <div class="body">
+    </div>
+  </div>
+</template>
+
+<script>
+  import subTitle from "@/components/subTitle"
+  import mixin from "@/utils/mixin"
+  export default {
+    components: {
+      subTitle,
+    },
+    mixins: [mixin],
+    data(){
+      return{
+
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  @import "~@/assets/css/common";
+</style>
